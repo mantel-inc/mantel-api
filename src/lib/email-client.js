e309d52e0200ee318c9385a7f8b4516a3147bfb7
Eric Miller <epmiller8464@users.noreply.github.com>
2023-03-18 22:09:16 -0500

    fix

--- src/lib/email-client.js
+++ src/lib/email-client.js
@@ -3,8 +3,8 @@ import sgMail from '@sendgrid/mail'
 sgMail.setApiKey(process.env.SEND_GRID)
 
 const msg = {
-  to: ['epmiller8464@gmail.com','zac@usemantel.com','zdearing@gmail.com'], // Change to your recipient
-  from: 'epmiller8464@gmail.com', // Change to your verified sender
+  to: ['eric.p.miller@gmail.com','zac@usemantel.com','zdearing@gmail.com'], // Change to your recipient
+  from: 'eric.p.miller@gmail.com', // Change to your verified sender
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
