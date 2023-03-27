4b11fff07604a8f34b27531373c95de4dec9ca7a
Eric Miller <epmiller8464@users.noreply.github.com>
2023-03-26 22:49:41 -0500

    feat: joi validation

--- src/lib/analytics/validators.js
+++ src/lib/analytics/validators.js
@@ -1,5 +1,13 @@
 import { EventTypes } from './constants.js'
+import joi from 'joi'
 
-export const validateEventType = (eventType) => {
-  return Object.values(EventTypes).includes(eventType)
-}
+export const activityEventSchema = joi.object({
+  sessionId: joi.string().required(),
+  userId: joi.string().required(),
+  contractorId: joi.string().required(),
+  eventType: joi.string().allow(...Object.values(EventTypes)).required(),
+  data: joi.object().required(),
+  source: joi.string().required(),
+  path: joi.string().required(),
+})
+
--- src/lib/analytics/validators.test.js
+++ src/lib/analytics/validators.test.js
@@ -1,5 +1,30 @@
-import { test } from '@jest/globals'
+import { test, expect } from '@jest/globals'
 
-import { validateEventType } from './validators.js'
+import { activityEventSchema } from './validators.js'
 
-test('', () => {})
+test('validateEventType: positive', () => {
+  const { error, value } = activityEventSchema.validate({
+    sessionId: '1',
+    userId: '1',
+    contractorId: '1',
+    eventType: 'page-view',
+    data: {},
+    source: '1',
+    path: '1',
+  })
+  console.log(error)
+  expect(error).toBeUndefined()
+})
+test('validateEventType: negative', () => {
+  const { error, value } = activityEventSchema.validate({
+    sessionId: '1',
+    userId: '1',
+    contractorId: null,
+    eventType: 'age-view',
+    data: '',
+    source: '1',
+    path: '1',
+  }, { abortEarly: false })
+  console.log(error)
+  expect(error).toBeUndefined()
+})
