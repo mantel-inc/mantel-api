2bd66631f52d0fb43480fb824b2c0c046d6a9f8f
Eric Miller <epmiller8464@users.noreply.github.com>
2023-04-05 22:04:26 -0500

    feat: email updates

--- src/lib/email-client.js
+++ src/lib/email-client.js
@@ -4,28 +4,32 @@ import yaml from 'json-to-pretty-yaml'
 sgMail.setApiKey(process.env.SEND_GRID)
 
 let msg = {
-  // to: ['epmiller8464@gmail.com'], // Change to your recipient
-  to: ['epmiller8464@gmail.com', 'zac@usemantel.com', 'zdearing@gmail.com'], // Change to your recipient
-  // to: ['eric.p.miller84@gmail.com','zac@usemantel.com','zdearing@gmail.com'], // Change to your recipient
-  // to: ['eric.p.miller84@gmail.com'], // Change to your recipient
-  from: 'eric@usemantel.com', // Change to your verified sender
-  subject: 'Mantel Customer Survey Results',
-  // text: 'and easy to do anywhere, even with Node.js',
-  // html: '<code>and easy to do anywhere, even with Node.js</code>',
+    // to: ['epmiller8464@gmail.com'], // Change to your recipient
+    to: ['epmiller8464@gmail.com', 'zac@usemantel.com'], // Change to your recipient
+    // to: ['eric.p.miller84@gmail.com','zac@usemantel.com','zdearing@gmail.com'], // Change to your recipient
+    // to: ['eric.p.miller84@gmail.com'], // Change to your recipient
+    from: 'eric@usemantel.com', // Change to your verified sender
+    subject: 'Mantel Customer Survey Results',
+    // text: 'and easy to do anywhere, even with Node.js',
+    // html: '<code>and easy to do anywhere, even with Node.js</code>',
 }
 
 const sendEmail = async (data) => {
-  console.log('send email')
-  const { to, body } = data
-  try {
-    console.log(yaml.stringify(body))
-    msg.to = to || ['epmiller8464@gmail.com', 'zac@usemantel.com']
-    msg.text = yaml.stringify(body)
-    await sgMail.send(msg)
-    console.log('Email sent')
-  } catch (error) {
-    console.error(error)
-  }
+    console.log('send email')
+    const {to, body} = data
+    try {
+        // console.log(yaml.stringify(body))
+        msg.template_id = 'd-265205be7ef94c3088b8bee3999e0c35'
+        msg.personalizations = [{
+            to: [{email: 'epmiller8464@gmail.com'}],
+            dynamic_template_data: body
+        }]
+        
+        await sgMail.send(msg)
+        console.log('Email sent')
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 export default sendEmail
