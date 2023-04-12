852c45cca3484ab29f1685666105467f3c4e0731
Eric Miller <epmiller8464@users.noreply.github.com>
2023-04-11 21:16:15 -0500

    change contractor seed for now

--- src/data/contractor.js
+++ src/data/contractor.js
@@ -2,7 +2,8 @@ const Contractor = {
     display_full_name: 'McCullough Heating & Air Conditioning',
     display_name: 'McCullough',
     region: ['south'],
-    scout_report_email_recipients: ['al@coolmenow.com'],
+    // scout_report_email_recipients: ['al@coolmenow.com'],
+    scout_report_email_recipients: ['eric@usemantel.com'],
     url_path_name: 'mccullough',
     main_website_url: 'https://coolmenow.com/',
     display_small_logo_url:''
