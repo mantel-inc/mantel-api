c7a6c3350836fe3bdc1d3b07f4e08bed70fc0800
Eric Miller <epmiller8464@users.noreply.github.com>
2023-04-13 18:46:39 -0500

    fix: removed sync force

--- src/lib/database/db.js
+++ src/lib/database/db.js
@@ -156,7 +156,6 @@ const connect = async () => {
         await sequelize.authenticate()
         console.log('Connection has been established successfully.')
         await sequelize.sync()
-        // await sequelize.sync({force: true})
         
     } catch (error) {
         console.error('Unable to connect to the database:', error)
